a4b7b6a58ce84053bc4873462c21fbdbf37df31e
7kzaincode <127576967+7kzaincode@users.noreply.github.com>
2026-01-15 20:28:29 -0500

    added favicon

--- constants.ts
+++ constants.ts
@@ -101,7 +101,8 @@ export const SOCIAL_LINKS: SocialLink[] = [
   { label: "linkedin", url: "https://www.linkedin.com/in/zainkkhan/" },
   { label: "github", url: "https://github.com/7kzaincode" },
   { label: "mail", url: "mailto:kn.zain@hotmail.com" },
-  { label: "twitter", url: "https://x.com/sevenkzain" }
+  { label: "twitter", url: "https://x.com/sevenkzain" },
+  { label: "resume", url: "/resume.pdf" }
 ];
 
 export const ARTWORK = [
